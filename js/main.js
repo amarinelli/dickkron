318628fa444c8f5954060c30f5c2687e4c8bef24
Adam <amarinelli@esri.ca>
2015-01-17 01:31:49 -0500

    Accepting url parameter from form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
+
   console.log("ready!");
+
+
+    var user = location.search.split("=");
+    console.log(user[1]);
+    $('#name').html('Hi ' + user[1]);
+
 });
